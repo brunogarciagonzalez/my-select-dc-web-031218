61b44e0079650f6e708d64edd2454345bdca630f
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-02-24 18:59:31 -0500

    lab completed

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
  # your code here!
+ counter = 0
+ modified_collection = []
+ while counter < collection.length
+   if (yield collection[counter])
+     modified_collection.push(collection[counter])
+   end
+   counter += 1
+ end
+
+ return modified_collection
+
 end
